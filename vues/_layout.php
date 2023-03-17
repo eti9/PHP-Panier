c6180b1139fd6f0b4a437e95a25b6332640c4f58
Etienne Robert <etienno.robert@hotmail.com>
2023-03-16 21:45:13 -0400

    first commit

---
+++ vues/_layout.php
@@ -0,0 +1,20 @@
+<!-- THIS IS A PHP PROJECT TO SELECT ITEM AND --
+  --    SAVE THEM IN A SHOPPING CART WITH     --
+  --        AUTHENTIFICATION.                 -->
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <?= $content ?>
+</body>
+<html>
